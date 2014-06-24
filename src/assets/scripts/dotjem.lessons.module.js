7322cfb401d3f5e747cd716d5c6461f90b387d3e
Jens Melgaard <admin@it-links.dk>
2014-06-24 23:24:23 +0200

    lessons layout and styling

--- src/assets/scripts/dotjem.lessons.module.js
+++ src/assets/scripts/dotjem.lessons.module.js
@@ -1,22 +1,35 @@
 angular.module('dotjem.lessons', ['dotjem.routing']);
 
 angular.module('dotjem.lessons')
-    .config(['$stateProvider', function(sp) {
-    sp.state('home', {
-        route: '/',
-        views: {
-            main: {
-                template: 'assets/templates/home.html'
+    .config(['$stateProvider', function (sp) {
+        sp.state('home', {
+            route: '/',
+            views: {
+                main: {
+                    template: 'assets/templates/home.html'
+                }
             }
-        }
-    })
+        });
 
-}]);
+        sp.state('lessons', {
+            route: '/lessons',
+            views: {
+                main: {
+                    template: 'assets/templates/lessons/lessons.html'
+                }
+            }
+        });
+
+        sp.state('lessons.forms', {
+            route: '/forms',
+            views: {
+                main: {
+                    template: 'assets/templates/lessons/form-validation/form-validation.html'
+                }
+            }
+        })
+    }]);
 
 angular.module('dotjem.lessons')
-    .controller('appController', ['$scope', function(scope){
-
-
-
-
-}]);
+    .controller('appController', ['$scope', function (scope) {
+    }]);
