c4b4f327e6345b809ec441844e366d51b01b0494
Jens Melgaard <admin@it-links.dk>
2014-06-24 21:27:19 +0200

    basic stuff still

--- src/assets/scripts/dotjem.lessons.module.js
+++ src/assets/scripts/dotjem.lessons.module.js
@@ -1,5 +1,17 @@
 var dotjem = angular.module('dotjem.lessons', ['dotjem.routing']);
 
+dotjem.config(['$stateProvider', function(sp) {
+    sp.state('home', {
+        route: '/',
+        views: {
+            main: {
+
+            }
+        }
+    })
+
+}]);
+
 dotjem.controller('appController', ['$scope', function(scope){
 
 
